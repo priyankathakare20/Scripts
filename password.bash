f1bdbd1c6888a945a650b2e13dc542f09feafb2b
priyankathakare20 <67258192+priyankathakare20@users.noreply.github.com>
2020-06-22 12:51:20 +0530

    Create password.bash
    Bash script for show new password

---
+++ password.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+unset username
+unset password
+echo -n "username:"
+read username
+prompt="password:"
+while IFS= read -p "$prompt" -r -s -n 1 char
+do
+    if [[ $char == $'\0' ]]
+    then
+         break
+    fi
+    prompt='*'
+    password+="$char"
+done
